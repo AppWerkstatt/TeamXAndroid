dc77804f727c7c3b1b7f40c5be6c1572369cad61
Lukas Boehler <lukasboehler@Lukass-MacBook-Pro.local>
2017-05-14 14:46:16 +0200

    Added flyto

---
+++ app/src/main/java/at/fhv/teamx/wikigeoinfo/FlyToActivity.java
@@ -0,0 +1,19 @@
+package at.fhv.teamx.wikigeoinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class FlyToActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fly_to);
+
+        ListView flytoListview = (ListView)findViewById(R.id.flytoListview);
+        FirebaseListAdapter
+    }
+}
